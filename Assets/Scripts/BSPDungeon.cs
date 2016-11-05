43f1e7f0deedb2aa079ceb9bbc9e1b4812edb60b
dgoldaraz <dgoldaraz@gmail.com>
2016-11-05 16:08:53 +0000

    Create BSP Tree

--- Assets/Scripts/BSPDungeon.cs
+++ Assets/Scripts/BSPDungeon.cs
@@ -8,18 +8,27 @@ using System.Collections.Generic;
 
 public class BSPDungeon : MonoBehaviour
 {
-
-    private Room m_root;
-    private List<Room> leafNodes;
+    private GameObject m_root;
+   // private List<Room> leafNodes;
 
     public float initialW = 100;
     public float initialH = 100;
     public int divisionTimes = 1;
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        //Create mainRoom
+        m_root = new GameObject("Root");
+        m_root.transform.SetParent(transform);
+        Room roomComponent = m_root.AddComponent<Room>();
+        //Instantiate(roomTemplate, transform.position, Quaternion.identity) as Room;
+        roomComponent.SetWidth(initialW);
+        roomComponent.SetHeight(initialH);
+        roomComponent.SetCenter(transform.position);
+        DivideRooms();
+        roomComponent.Create();
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -35,7 +44,7 @@ public class BSPDungeon : MonoBehaviour
         {
             if (r.IsLeaf())
             {
-                leafs.Remove(r.GetParent());
+                leafs.Remove(r.GetParent().GetComponent<Room>());
                 leafs.Add(r);
             }
         }
@@ -45,7 +54,7 @@ public class BSPDungeon : MonoBehaviour
     {
         for(int i = 0; i < divisionTimes; i++)
         {
-            m_root.Divide();
+            m_root.GetComponent<Room>().Divide();
         }
     }
 
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class Room : MonoBehaviour {
 
 
-    private Room m_parent;
-    private Room m_lChild;
-    private Room m_rChild;
+    private GameObject m_parent;
+    private GameObject m_lChild;
+    private GameObject m_rChild;
 
-    private float m_width;
-    private float m_height;
+    private float m_width = 100.0f;
+    private float m_height = 100.0f;
+    private Vector3 m_center;
+
+    private GameObject m_quad;
 	
 
     public bool IsLeaf()
@@ -32,34 +35,87 @@ public class Room : MonoBehaviour {
         return m_height;
     }
 
-    public Room GetParent()
+    public void SetWidth(float w)
+    {
+        m_width = w;
+    }
+
+    public void SetHeight(float h)
+    {
+        m_height = h;
+    }
+
+    public Vector3 GetCenter()
+    {
+        return m_center;
+    }
+
+    public void SetCenter(Vector3 c)
+    {
+        m_center = c;
+    }
+
+    public GameObject GetParent()
     {
         return m_parent;
     }
 
-    public Room GetLChildren()
+    public GameObject GetLChild()
     {
         return m_lChild;
     }
 
-    public Room GetRChildren()
+    public GameObject GetRChild()
     {
         return m_rChild;
     }
 
-    public void SetParent(Room p)
+    void CreateChild(Vector3 center,float w, float h, bool left)
+    {
+        Room component = null;
+        if (left)
+        {
+            if (m_lChild == null)
+            {
+                m_lChild = new GameObject("LChild");
+                m_lChild.transform.SetParent(transform);
+                component = m_lChild.AddComponent<Room>();
+            }
+        }
+        else
+        {
+            if (m_rChild == null)
+            {
+                m_rChild = new GameObject("RChild");
+                m_rChild.transform.SetParent(transform);
+                component = m_rChild.AddComponent<Room>();
+            }
+        }
+        if(component)
+        {
+            component.SetParent(gameObject);
+            component.SetCenter(center);
+            component.SetWidth(w);
+            component.SetHeight(h);
+        }
+    }
+
+    public void SetParent(GameObject p)
     {
         m_parent = p;
+        transform.SetParent(p.transform);
     }
 
-    public void SetLChild(Room c)
+    public void SetLChild(GameObject c)
     {
         m_lChild = c;
+        m_lChild.transform.SetParent(transform);
     }
 
-    public void SetRChild(Room c)
+    public void SetRChild(GameObject c)
     {
         m_rChild = c;
+        m_rChild.transform.SetParent(transform);
     }
 
     public void Divide()
@@ -68,11 +124,88 @@ public class Room : MonoBehaviour {
         if(IsLeaf())
         {
             //Divide in two new rooms
+            //Get a Random Direction
+            if(Random.value > 0.5f)
+            {
+                //Horizontal
+                DivideHorizontally();
+            }
+            else
+            {
+                //Vertical
+                DivideVertically();
+            }
         }
         else
         {
-            m_lChild.Divide();
-            m_rChild.Divide();
+            m_lChild.GetComponent<Room>().Divide();
+            m_rChild.GetComponent<Room>().Divide();
+        }
+    }
+
+    /// <summary>
+    /// Divide the room in a random Horizontal positions (random Y value)
+    /// </summary>
+    void DivideHorizontally()
+    {
+        //Calculate offset
+        int offset = (int)(m_height * 0.1);
+        //if(m_height - offset * 2 > 5)
+        {
+            //We are able to create a room
+            float randomY = Random.Range(offset, (int)m_height - offset);
+            //Divide and create two new rooms
+
+            Vector3 bottom = m_center - new Vector3(0.0f, m_height * 0.5f, 0.0f);
+            float lHeight = randomY;
+            float rHeight = m_height - randomY;
+
+            Vector3 upCenter = bottom + new Vector3(0.0f, rHeight + lHeight * 0.5f, 0.0f);
+            Vector3 downCenter = bottom + new Vector3(0.0f, rHeight * 0.5f, 0.0f);
+
+            CreateChild(upCenter, m_width, lHeight, true);
+            CreateChild(downCenter, m_width, rHeight, false);
+        }
+    }
+
+    /// <summary>
+    /// Divide the room in a random Horizontal positions (random X value)
+    /// </summary>
+    void DivideVertically()
+    {
+        //Calculate offset
+        int offset = (int)(m_width * 0.1);
+        //if (m_width - offset * 2 > 20)
+        {
+            //We are able to create a room
+            float randomX = Random.Range(offset, (int)m_width - offset);
+            //Divide and create two new rooms
+            float lWidth = randomX;
+            float rWidth = m_width - randomX;
+            Vector3 leftMost = m_center - new Vector3(m_width * 0.5f, 0.0f, 0.0f);
+
+            Vector3 lCenter = leftMost + new Vector3(lWidth * 0.5f, 0.0f, 0.0f);
+            Vector3 rCenter = leftMost + new Vector3(lWidth + rWidth * 0.5f , 0.0f, 0.0f);
+
+            CreateChild(lCenter, lWidth, m_height, true);
+            CreateChild(rCenter, rWidth, m_height, false);
+        }
+    }
+
+    public void Create()
+    {
+        //Create a quad and give the position and w/h
+        if(IsLeaf())
+        {
+            m_quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            m_quad.transform.SetParent(transform);
+            m_quad.transform.position = m_center;
+            m_quad.transform.localScale = new Vector3(m_width, m_height, 1);
+        }
+        else
+        {
+            m_lChild.GetComponent<Room>().Create();
+            m_rChild.GetComponent<Room>().Create();
         }
     }
 }
