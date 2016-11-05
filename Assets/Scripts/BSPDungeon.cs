c08f7b71b86fb760d8b88a0c9f8f55ec852d0186
dgoldaraz <dgoldaraz@gmail.com>
2016-11-05 12:04:35 +0000

    Add Basics

---
+++ Assets/Scripts/BSPDungeon.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class that generates a BSP Dungeon following http://www.roguebasin.com/index.php?title=Basic_BSP_Dungeon_generation
+/// </summary>
+
+public class BSPDungeon : MonoBehaviour
+{
+
+    private Room m_root;
+    private List<Room> leafNodes;
+
+    public float initialW = 100;
+    public float initialH = 100;
+    public int divisionTimes = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void AddLeafs(List<Room> leafs)
+    {
+        //Add new leafs to the list
+        //Remove parent if it's contained
+        foreach(Room r in leafs)
+        {
+            if (r.IsLeaf())
+            {
+                leafs.Remove(r.GetParent());
+                leafs.Add(r);
+            }
+        }
+    }
+
+    void DivideRooms()
+    {
+        for(int i = 0; i < divisionTimes; i++)
+        {
+            m_root.Divide();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour {
+
+
+    private Room m_parent;
+    private Room m_lChild;
+    private Room m_rChild;
+
+    private float m_width;
+    private float m_height;
+	
+
+    public bool IsLeaf()
+    {
+        return m_lChild == null && m_rChild == null;
+    }
+
+    public bool IsRoot()
+    {
+        return m_parent == null;
+    }
+
+    public float GetWidth()
+    {
+        return m_width;
+    }
+
+    public float GetHeight()
+    {
+        return m_height;
+    }
+
+    public Room GetParent()
+    {
+        return m_parent;
+    }
+
+    public Room GetLChildren()
+    {
+        return m_lChild;
+    }
+
+    public Room GetRChildren()
+    {
+        return m_rChild;
+    }
+
+    public void SetParent(Room p)
+    {
+        m_parent = p;
+    }
+
+    public void SetLChild(Room c)
+    {
+        m_lChild = c;
+    }
+
+    public void SetRChild(Room c)
+    {
+        m_rChild = c;
+    }
+
+    public void Divide()
+    {
+        //Divide the room in another two rooms
+        if(IsLeaf())
+        {
+            //Divide in two new rooms
+        }
+        else
+        {
+            m_lChild.Divide();
+            m_rChild.Divide();
+        }
+    }
+}
